cc20b546b69d93031aa5214977ddabe4f12d6950
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-06-12 18:53:45 +0530

    Exercise 1.4 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -12,10 +12,11 @@ const Part = ({part, exercise}) => {
   )
 }
 
-const Content = ({ part1, part2, part3 }) => {
-  console.log({part1})
-  console.log({part2})
-  console.log({part3})
+const Content = ({ parts }) => {
+  console.log({parts})
+  let part1 = parts[0]
+  let part2 = parts[1]
+  let part3 = parts[2]
   return(
     <div>
         <Part part = {part1.name} exercise = {part1.exercises} />
@@ -34,14 +35,25 @@ const Total = ({total}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = { name:'Fundamentals of React', exercises: 10 }
-  const part2 = { name:'Using props to pass data', exercises: 7 }
-  const part3 = { name:'State of a component', exercises: 14 }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course = {course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} />
+      <Content parts = {parts} />
       {/* <Total total={part1.exercises + part2.exercises + part3.exercises} /> */}
     </div>
   )
