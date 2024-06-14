fba1c4ee6660ced2ad7c4d75c4c8c406b441eed6
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-06-14 20:27:51 +0530

    Added useState to page re-rendering part

--- fso_lesson_stuff/page_re-rendering/src/main.jsx
+++ fso_lesson_stuff/page_re-rendering/src/main.jsx
@@ -1,20 +1,7 @@
 
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-  refresh()
-counter+=1
-},5000)
-
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 
 
