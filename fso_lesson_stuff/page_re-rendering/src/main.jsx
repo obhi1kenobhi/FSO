4c3ea5cd32e8daa3240cddbfd6ea1cf60401b2a3
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-06-14 20:06:10 +0530

    Used setinterval to change page text

--- fso_lesson_stuff/page_re-rendering/src/main.jsx
+++ fso_lesson_stuff/page_re-rendering/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+  refresh()
+counter+=1
+},5000)
+
+
+
+
+
