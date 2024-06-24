b67e626ea1bad591e578fcaf38b435ab7ab2a135
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-06-24 20:35:00 +0530

    Page re-rendering lesson

--- fso_lesson_stuff/part1/component_state/component_state/src/App.jsx
+++ fso_lesson_stuff/part1/component_state/component_state/src/App.jsx
@@ -1,28 +1,8 @@
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
+export default App
